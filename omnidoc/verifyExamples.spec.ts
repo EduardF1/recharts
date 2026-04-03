80ec21528e439cd65540138f0d62946828e8b84c
Pavel Vanecek <corkscreewe@gmail.com>
2026-04-03 23:30:48 +0900

    Animation matching

--- omnidoc/verifyExamples.spec.ts
+++ omnidoc/verifyExamples.spec.ts
@@ -192,6 +192,9 @@ describe('Documentation Examples Coverage', () => {
     'NiceTicksAlgorithm',
     'EasingInput',
     'AnimationInterpolateFn',
+    'AnimationMatchBy',
+    'matchByIndex',
+    'matchByDataKey',
   ];
 
   describe.each(allExports.filter(name => !exportsThatNeedExamples.includes(name)))('Export: %s', exportName => {
