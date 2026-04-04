78473a9c21df44b7596895d3814582c873d685ea
Pavel Vanecek <corkscreewe@gmail.com>
2026-04-04 15:40:36 +0900

    Add AnimationProgressProvider and manual animation control features

--- omnidoc/verifyExamples.spec.ts
+++ omnidoc/verifyExamples.spec.ts
@@ -195,6 +195,10 @@ describe('Documentation Examples Coverage', () => {
     'AnimationMatchBy',
     'matchByIndex',
     'matchByDataKey',
+    'AnimationProgressProvider',
+    'useAnimationProgress',
+    'AnimationHandle',
+    'AnimationStatus',
   ];
 
   describe.each(allExports.filter(name => !exportsThatNeedExamples.includes(name)))('Export: %s', exportName => {
